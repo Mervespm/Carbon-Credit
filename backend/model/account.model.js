3a6ce450d219be05b0d04095f1da20cabe7961e5
Alonzo Velez <alonzovelez9@gmail.com>
2025-04-19 02:52:33 -0400

    Fixed company_id bug in account.model.js

--- backend/model/account.model.js
+++ backend/model/account.model.js
@@ -24,7 +24,7 @@ const accountSchema = mongoose.Schema(
     },
     company_id: {
       type: String,
-      required: () => {
+      required: function () {
         this.user_type == "employee";
       },
     },
