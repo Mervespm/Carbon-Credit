1e2754fcc72283749b835c4f10095d50a9f0968b
Merve Karabulut <mrvkrblt14@gmail.com>
2025-04-20 01:31:11 -0400

    Add server.js a link

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -106,8 +106,10 @@ export const validateCompanyCode = async (req, res) => {
 
 export const getProfile = async (req, res) => {
   try {
+    console.log("📦 Session at /me:", req.session);
     const user = await Account.findById(req.session.user.user_id);
-    console.log("Session data:", req.session);
+    
+
 
     if (!user) return res.status(404).json({ message: 'User not found' });
 
