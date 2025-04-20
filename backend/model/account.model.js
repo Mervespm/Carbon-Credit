ce23bff9ce06ae1752253ae567687da55780fde3
Merve Karabulut <mrvkrblt14@gmail.com>
2025-04-19 23:30:09 -0400

    syntax fixed

--- backend/model/account.model.js
+++ backend/model/account.model.js
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-import mongoose from "mongoose";
-
-const accountSchema = mongoose.Schema(
-  {
-    first_name: {
-      type: String,
-      required: [true, "Enter a first name"],
-    },
-    last_name: {
-      type: String,
-      required: [true, "Enter a last name"],
-    },
-    email: {
-      type: String,
-      required: [true, "Enter a valid email"],
-    },
-    password: {
-      type: String,
-      required: [true, "Enter a password"],
-    },
-    user_type: {
-      type: String,
-      required: [false, "All users must have a type"],
-      default: "customer",
-    },
-    carbon_credits: {
-      type: mongoose.SchemaTypes.ObjectId,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
-=======
 
 
 import mongoose from "mongoose";
@@ -65,7 +30,6 @@ const accountSchema = new mongoose.Schema({
   }
 });
 
->>>>>>> merv
 
 const Account = mongoose.model("Account", accountSchema);
 export default Account;
